3063a512a81c8a764913e88664bf955dda4f8124
Guilherme <mttsguil@gmail.com>
2022-09-20 08:41:48 -0300

    add: addEvent

--- src/template/IconButton.jsx
+++ src/template/IconButton.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import If from "./If";
 
 export default props => ( 
-    <If test={props.hide}>
+    <If test={!props.hide}>
           <button className={'btn btn-'+ props.style}
-            onClick={props.onClick}
-            >
+            onClick={props.onClick}>
                 <i className={'fa fa-'+ props.icon}></i>
             </button>
     </If>
