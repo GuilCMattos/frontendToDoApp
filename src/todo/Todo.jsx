e39cb9889766143704730f39322de6dcd92b9613
Guilherme <mttsguil@gmail.com>
2022-09-20 09:14:11 -0300

    add: consult and delete ToDo's

--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -17,6 +17,14 @@ export default class Todo extends Component {
 
         this.handleAdd = this.handleAdd.bind(this)
         this.handleChange = this.handleChange.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+
+        this.refresh()
+    }
+
+    refresh() { 
+        axios.get(`${URL}?sort=-createdAt`)
+        .then(resp=> this.setState({...this.state, description: '', list: resp.data}))
     }
 
     handleChange(e) { 
@@ -27,7 +35,12 @@ export default class Todo extends Component {
         const description = this.state.description
 
         axios.post(URL, {description})
-        .then(resp => console.log('Funcionou!'))
+        .then(resp => this.refresh())
+    }
+
+    handleRemove(todo) { 
+        axios.delete(`${URL}/${todo._id}`)
+        .then(resp=> this.refresh())
     }
 
 
@@ -40,7 +53,7 @@ export default class Todo extends Component {
                 handleAdd={this.handleAdd}
                 handleChange={this.handleChange}
                 />
-                <TodoList />
+                <TodoList list={this.state.list} handleRemove={this.handleRemove} />
             </div>
         )
     }
