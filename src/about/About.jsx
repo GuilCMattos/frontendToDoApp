dbded91244af97be9cd72514f99542fceb531014
Guilherme <mttsguil@gmail.com>
2022-09-20 08:00:51 -0300

    add: PageHeader component

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import PageHeader from '../template/PageHeader'
 
 const About = props => (
     <div>
-        <h1>Sobre</h1>
+       <PageHeader name="Sobre" small="Nós"/>
+       <h2>Nossa História</h2>
+       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores obcaecati reiciendis tempora quae similique vel cum inventore quod nisi excepturi iste debitis maxime, possimus optio est veniam error magnam pariatur!</p>
+
+       <h2>Missão e Visão</h2>
+       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus esse rem praesentium quisquam ad facere repudiandae sapiente incidunt fuga adipisci reiciendis doloremque ipsum, eius non impedit commodi obcaecati sint est.</p>
+
+       <h2>Imprensa</h2>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, dolorum. Est, doloribus debitis! Ad non labore numquam eaque officiis voluptate culpa delectus? Aliquam reprehenderit aut distinctio optio, vero voluptatum voluptate.</p>
     </div>
 )
 
